29ad6c78cc38376127f4f51002f30a8aa00db24b
jyywong <wong.jonathan1@gmail.com>
2021-06-12 21:27:15 -0400

    Added allowed heroku website to allowed hosts

--- moonMS/settings.py
+++ moonMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nameless-chamber-50852.herokuapp.com', '127.0.0.1:8000']
 
 CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
